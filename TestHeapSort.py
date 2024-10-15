3b7fc624110e1734dacb52e9c8c24052429f2e2a
EnyerSilva <18-11100@usb.ve>
2024-10-15 16:35:59 -0400

    Agregando Segunda Parte del HeapSort

---
+++ TestHeapSort.py
@@ -0,0 +1,25 @@
+import sys
+import time
+from HeapSort import *
+
+archivo = sys.argv[1]
+file = open(archivo, "r")
+lineas = file.readlines()
+arreglo_inicial = []
+for k in lineas:
+    arreglo_inicial.append(int(k.strip()))
+
+n = int(len(arreglo_inicial))
+algoritmo = "HeapSort"
+inicio = time.time()
+arreglo_ordenado = Heapsort(arreglo_inicial)
+arreglo_strings = [str(elem) + "\n" for elem in arreglo_ordenado]
+fin = time.time()
+archivo2 = open("ArchivoNumeros.sorted" , "x")
+archivo2 = open("ArchivoNumeros.sorted" , "w")
+archivo2.writelines(arreglo_strings)
+archivo2.close()
+tiempo_Transcurrido = (fin-inicio) * 1000
+
+print(algoritmo, n, tiempo_Transcurrido)
+

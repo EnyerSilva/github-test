131dee75981032e86b7afdff1332c93e29074d2c
RenaC15 <rencolon62@gmail.com>
2024-10-15 17:00:44 -0400

    listo

--- heapsortr.py
+++ heapsortr.py
@@ -1,4 +1,5 @@
-def maxHeapify(A, n, i):
+def maxHeapify(A, i):
+    global n
     leftChild = 2*i + 1
     rightChild = 2*i + 2
 
@@ -11,18 +12,20 @@ def maxHeapify(A, n, i):
     
     if i != largest:
         A[i], A[largest] = A[largest], A[i]
-        maxHeapify(A, n, largest)
+        maxHeapify(A, largest)
 
 def buildMaxHeap(A):
-    n = len(A)
+    global n
     for i in range(n//2 - 1, -1, -1):
-        maxHeapify(A, n, i)
+        maxHeapify(A, i)
 
 
 def Heapsort(A):
+    global n
     n = len(A)
     buildMaxHeap(A)
     for i in range(n - 1, 0, -1):
         A[i], A[0] = A[0], A[i]
-        maxHeapify(A, i, 0)
+        n = n - 1
+        maxHeapify(A, 0)
     return A

15b9aab5ad0a03ab025274c2be17ea291fb95431
RenaC15 <rencolon62@gmail.com>
2024-10-15 19:25:02 -0400

    otra prueba

--- quicksort.py
+++ quicksort.py
@@ -1,26 +1,41 @@
 from typing import List
 
+def MergeSort(A, p, r):
 
-def Partition(A, p, r):
-    x = A[r]                                           #Elemento Pivote
-    i = p - 1
-    j = p
-    for j in range(p,r):
-        if (A[j] <= x):
+    if p < r:
+        q = (p + r) // 2
+        MergeSort(A, p, q)
+        MergeSort(A, q + 1, r)
+        Merge(A, p, q, r)
+
+
+def Merge(A, p, q, r):
+    n = q - p + 1
+    m = r - q
+    L = [0]*(n + 1)
+    R = [0]*(m + 1)
+    for i in range(n):
+        L[i] = A[p + i]
+    for j in range(m):
+        R[j] = A[q + 1 + j]
+    L[n] = float('inf')
+    R[m] = float('inf')
+    i = 0
+    j = 0
+    for k in range(p, r + 1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
             i += 1
-            A[i] , A[j] = A[j] , A[i]
-    A[i+1], A[r] = A[r], A[i + 1]
-    return i + 1
+        else:
+            A[k] = R[j]
+            j += 1
 
-def QuickSort (A, p, r):
-    if (p < r):
-        q = Partition(A, p, r)     
-        QuickSort(A, p, q - 1)
-        QuickSort(A, q + 1, r)
-        return A
-    else:
-        return A
 
-arr = [100,200,500,300,1000,900,800,700,600,3,2,1,0,-1,-2]
-print(arr)
-print(QuickSort(arr,0,len(arr)-1))
+def InsertionSort(A, p, r):
+    for j in range(1, r+1):
+        actual = A[j]
+        i = j - 1
+        while i >= p and A[i] > actual:
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = actual

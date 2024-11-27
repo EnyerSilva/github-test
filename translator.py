eed36c1a63302e955e749f83effeae1485209f31
EnyerSilva <18-11100@usb.ve>
2024-11-27 11:21:48 -0400

    Traductor listo

--- translator.py
+++ translator.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def strtoascii(p:str):
     if type(p) == int:
         raise Exception("Ingrese solo texto")
@@ -12,3 +14,20 @@ def strtoascii(p:str):
             lista.append(ord(p[i]))
             i += 1
     return lista
+
+def asciitostr(arr:List[int]):
+    '''
+    Recibe una lista de números de caracteres en formato ASCII
+    Y retorna la palabra o caracter original
+    '''
+    for i in range(len(arr)):
+        if type(arr[i]) == str:
+            raise Exception("Solo se admiten numeros enteros")
+        elif type(arr[i]) == float:
+            raise Exception("Solo se admiten numeros enteros")
+        else:
+            arr[i] = chr(arr[i])
+    return ''.join(arr)
+
+print(asciitostr(strtoascii("Parangaricutinimicuaro")))
+         

28775c6a9923d8262d7d917675b560a1f4fb8421
RenaC15 <rencolon62@gmail.com>
2024-11-27 21:47:33 -0400

    lo que llevo por los momentos

---
+++ arbolbinario.py
@@ -0,0 +1,69 @@
+from typing import List
+
+class Nodo(object):
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class Tree(object):
+    def __init__(self):
+        self.root = None
+    
+    def tree_insert(self, z):
+        y = None
+        x = self.root
+        while x != None:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z.parent = y
+        if y == None:
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+
+    def inorder_tree_walk(self, x):
+        if x != None:
+            self.inorder_tree_walk(x.left)
+            print(x.key)
+            self.inorder_tree_walk(x.right)
+
+    def preorder_tree_walk(self, x):
+        if x != None:
+            print(x.key)
+            self.preorder_tree_walk(x.left)
+            self.preorder_tree_walk(x.right)
+
+    def postorder_tree_walk(self, x):
+        if x != None:
+            self.postorder_tree_walk(x.left)
+            self.postorder_tree_walk(x.right)
+            print(x.key)
+
+tree = Tree()
+tree.tree_insert(Nodo(12))
+tree.tree_insert(Nodo(5))
+tree.tree_insert(Nodo(18))
+tree.tree_insert(Nodo(2))
+tree.tree_insert(Nodo(9))
+tree.tree_insert(Nodo(15))
+tree.tree_insert(Nodo(19))
+tree.tree_insert(Nodo(13))
+tree.tree_insert(Nodo(17))
+
+#root = Nodo(1)
+#root.left = Nodo(2)
+#root.right = Nodo(3)
+#root.left.left = Nodo(4)
+#root.left.right = Nodo(5)
+tree.inorder_tree_walk(tree.root)
+print("\n")
+tree.preorder_tree_walk(tree.root)
+print("\n")
+tree.postorder_tree_walk(tree.root)
